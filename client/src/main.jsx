48747f9f0d5e8d57b09f1d7e185f9ec1aa30f4e4
Bodhisatta Bhattacharjee <bodhisatta1999@gmail.com>
2024-02-07 23:11:02 +0530

    fixed the routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { BrowserRouter, RouterProvider, createBrowserRouter, Routes, Route } from 'react-router-dom'
 // import App from './App.jsx'
 import './index.css'
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
 import Login from './components/Login/Login.jsx'
 import Signup from './components/Signin/Signup.jsx'
 import Layout from './Layout.jsx'
@@ -17,95 +19,112 @@ import Chat from './components/Dashboard/Chat.jsx'
 import Activity from './components/Dashboard/Activity.jsx'
 import Settings from './components/Dashboard/Settings.jsx'
 
-// import { AuthContext } from './components/Backend/context/auth-context.js'
-// import { useAuth } from './components/Backend/hooks/auth-hook.js'
+import { AuthContext } from './components/Backend/context/auth-context.js'
+import { useAuth } from './components/Backend/hooks/auth-hook.js'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: '/',
-        element: <Login />
-      },
-      {
-        path: 'signup',
-        element: <Signup />
-      },
-      {
-        path: 'dashboard',
-        element: [
-          <Sidebar />,
-          <Dashboard />
-        ],
-      },
-      {
-        path: 'employee',
-        element: [
-          <Sidebar />,
-          <Employee />
-        ]
-      },
-      {
-        path: 'projects',
-        element: [
-          <Sidebar />,
-          <Projects />
-        ]
-      },
-      {
-        path: 'termination',
-        element: [
-          <Sidebar />,
-          <Termination />
-        ]
-      },
-      {
-        path: 'internships',
-        element: [
-          <Sidebar />,
-          <Internship />
-        ]
-      },
-      {
-        path: 'jobs',
-        element: [
-          <Sidebar />,
-          <Jobs />
-        ]
-      },
-      {
-        path: 'chat',
-        element: [
-          <Sidebar />,
-          <Chat />
-        ]
-      },
-      {
-        path: 'activity',
-        element: [
-          <Sidebar />,
-          <Activity />
-        ]
-      },
-      {
-        path: 'settings',
-        element: [
-          <Sidebar />,
-          <Settings />
-        ]
-      }
+// const { token, login, logout, userId, isEmployee, isAdmin, userName } = useAuth();
 
-    ]
-  }
-])
+function App() {
+  const { token, login, logout, userId, isEmployee, isAdmin, userName } = useAuth();
 
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: !!token,
+        token: token,
+        userId: userId,
+        userName: userName,
+        isEmployee: isEmployee,
+        isAdmin: isAdmin,
+        login: login,
+        logout: logout
+      }}
+    >
+      <BrowserRouter>
+        <Header />
+        <div>
+        {token ? (
+          <>
+          <Sidebar />
+          {/* {!isEmployee && !isAdmin && (
+            <div>
+              <div>
+                <Routes>
+                  <Route exact path="/dashboard" element={<Dashboard />} />
+                  <Route exact path="/employee" element={<Employee />} />
+                  <Route exact path="/projects" element={<Projects />} />
+                  <Route exact path="/termination" element={<Termination />} />
+                  <Route exact path="/internships" element={<Internship />} />
+                  <Route exact path="/jobs" element={<Jobs />} />
+                  <Route exact path="/chat" element={<Chat />} />
+                  <Route exact path="/activity" element={<Activity />} />
+                  <Route exact path="/settings" element={<Settings />} />
+                </Routes>
+              </div>
+            </div>
+          )}
+          {isEmployee && !isAdmin && (
+            <div>
+              <div>
+                <Routes>
+                  <Route exact path="/auth" element={<Auth />} />
+                  <Route exact path="/dashboard" element={<Dashboard />} />
+                  <Route exact path="/edit/:uid" element={<EditDetails />} />
+                  <Route exact path="/internships" element={<Internships />} />
+                  <Route exact path="/jobs" element={<Jobs />} />
+                  <Route exact path="/projects" element={<Projects />} />
+                  <Route exact path="/terminations" element={<Terminations />} />
+                  <Route exact path="/leaves" element={<Leaves />} />
+                </Routes>
+              </div>
+            </div>
+          )}
+          {isAdmin && (
+            <div>
+              <div>
+                <Routes>
+                  <Route exact path="/auth" element={<Auth />} />
+                  <Route exact path="/dashboard" element={<Dashboard />} />
+                  <Route exact path="/employees" element={<Employees />} />
+                  <Route exact path="/internships" element={<Internships />} />
+                  <Route exact path="/jobs" element={<Jobs />} />
+                  <Route exact path="/projects" element={<Projects />} />
+                  <Route exact path="/terminations" element={<Terminations />} />
+                  <Route exact path="/leaves" element={<Leaves />} />
+                </Routes>
+              </div>
+            </div>
+          )} */}
+            <Routes>
+              <Route exact path="/" element={<Dashboard />} />
+              <Route exact path="/dashboard" element={<Dashboard />} />
+              <Route exact path="/employee" element={<Employee />} />
+              <Route exact path="/projects" element={<Projects />} />
+              <Route exact path="/termination" element={<Termination />} />
+              <Route exact path="/internships" element={<Internship />} />
+              <Route exact path="/jobs" element={<Jobs />} />
+              <Route exact path="/chat" element={<Chat />} />
+              <Route exact path="/activity" element={<Activity />} />
+              <Route exact path="/settings" element={<Settings />} />
+            </Routes>
+          </>
+        ) : (
+          // Default routes
+          <Routes>
+            <Route exact path="/" element={<Login />} />
+            <Route exact path="signup" element={<Signup />} />
+          </Routes>
+        )}</div> 
+        
+        <Footer />
+      </BrowserRouter>
 
+    </AuthContext.Provider>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-  < React.StrictMode >
-    <RouterProvider router={router} />
+  <React.StrictMode >
+    <App />
   </React.StrictMode >
 )
