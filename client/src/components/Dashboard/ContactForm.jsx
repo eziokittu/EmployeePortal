bd3660d8bb4f7bccb469499b09fc39de74c0bb0c
Bodhisatta Bhattacharjee <bodhisatta1999@gmail.com>
2024-02-22 00:49:41 +0530

    contact page done

--- client/src/components/Dashboard/ContactForm.jsx
+++ client/src/components/Dashboard/ContactForm.jsx
@@ -1,44 +1,79 @@
 import React, { useContext, useState } from 'react';
-import { useForm, ValidationError } from '@formspree/react';
 import { AuthContext } from '../Backend/context/auth-context';
 
 function ContactForm() {
   const auth = useContext(AuthContext);
 
-  const [inputEmail, setInputEmail] = useState(auth.email);
-  const [inputMessage, setInputMessage] = useState('Enter your message');
-  const [inputName, setInputName] = useState(auth.firstname+' '+auth.lastname);
+  const [state, setState] = useState({
+    name: auth.firstname+' '+auth.lastname,
+    email: auth.email,
+    message: ''
+  });
 
-  const [state, handleSubmit] = useForm("mjvnwzko");
-  if (state.succeeded) {
-    return <p>Thanks for joining!</p>;
-  }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setState(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  // Function to submit mail using formspree
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formAction = 'https://formspree.io/f/mjvnwzko'; // replace with other id
+    fetch(formAction, {
+      method: 'POST',
+      body: JSON.stringify(state),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+    })
+    .then(response => {
+      if (response.ok) {
+        alert('Message sent successfully.');
+        setState({
+          name: auth.firstname+' '+auth.lastname,
+          email: auth.email,
+          message: ''
+        });
+      } else {
+        alert('There was a problem with your submission. Please try again.');
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      alert('An error occurred. Please try again later.');
+    });
+  };
 
   return (
     
     <div className="p-4 sm:ml-64">
       <div className="p-4 border-2 border-gray-200 rounded-lg dark:border-gray-700">
         <section id="form" className="bg-white rounded-lg flex flex-col items-center gap-4">
-          <h1 className="text-3xl bg-white">Contact Form</h1>
-          <p className="bg-white text-base">Fill out the form below and we will get back to you.</p>
 
+          {/* Heading */}
+          <h1 className="text-3xl ">Contact Form</h1>
+          <p className=" text-base">Fill out the form below and we will get back to you.</p>
+
+          {/* Contact Form */}
           <form 
             onSubmit={handleSubmit} 
             id="contact" 
             className="bg-white min-w-[40rem] min-h-[25rem] p-4 flex flex-col justify-between"
           >
-            {/* <div className="input bg-white">
-              <p className="bg-white">Name</p>
-              <input type="text" name="name" id="name" className="bg-white rounded-md h-6 border border-gray-400 w-full mt-1 p-2"/>
-            </div> */}
-
+            {/* Name */}
             <div className="mb-4 relative">
               <label htmlFor="contact_name">Name</label>
               <input
-                onChange={(event) => setInputName(event.target.value)}
+                // onChange={(event) => setInputName(event.target.value)}
+                value={state.name}
+                onChange={handleChange}
                 type="text"
                 id="contact_name"
-                placeholder={inputName}
+                placeholder={auth.firstname+' '+auth.lastname}
                 className="block w-full h-10 mt-1 pl-10 border-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
               />
               <svg
@@ -47,18 +82,15 @@ function ContactForm() {
               </svg>
             </div>
 
-            {/* <div className="input bg-white">
-              <p className="bg-white">Email</p>
-              <input type="email" name="email" id="email" className="bg-white rounded-md h-6 border border-gray-400 w-full mt-1 p-2"/>
-            </div> */}
-
+            {/* Email */}
             <div className="mb-4 relative">
               <label htmlFor="contact_email">Email Address</label>
               <input
-                onChange={(event) => setInputEmail(event.target.value)}
+                value={state.email}
+                onChange={handleChange}
                 type="email"
                 id="contact_email"
-                placeholder={inputEmail}
+                placeholder={auth.email}
                 className="block w-full h-10 mt-1 pl-10 border-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
               />
               <svg
@@ -67,29 +99,31 @@ function ContactForm() {
               </svg>
             </div>
 
-            {/* <div className="input bg-white">
-              <p className="bg-white">Message</p>
-              <textarea rows="10" placeholder="Write your message here." className="bg-white rounded-md resize-none overflow-hidden h-36 border border-gray-400 w-full mt-1 p-2"></textarea>
-            </div> */}
-
+            {/* Message */}
             <div className='mb-4'>
               <label htmlFor="contact_message">Message</label>
               <textarea
-                onChange={(event) => setInputMessage(event.target.value)}
+                name='message'
+                value={state.message}
+                onChange={handleChange}
                 id="contact_message"
                 rows="6"
-                placeholder={inputMessage}
+                required
+                placeholder={"Enter your message"}
                 className="block p-2 w-full mt-1 border-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
               ></textarea>
             </div>
 
+            {/* Submit Button */}
             <button 
               type="submit" 
               disabled={state.submitting} 
               className="w-40 text-white bg-purple-700 border-none h-10 rounded-lg font-semibold text-base my-0 mx-auto transition-all duration-400 hover:bg-purple-900 hover:w-44"
               >Submit
             </button>
+
           </form>
+          
         </section>
       </div>
     </div>
