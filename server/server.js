4cb9a3c8f9807124712035f3bbc74b25791ab045
Bodhisatta Bhattacharjee <bodhisatta1999@gmail.com>
2024-03-03 23:42:22 +0530

    ll

--- server/server.js
+++ server/server.js
@@ -61,19 +61,47 @@ app.use((error, req, res, next) => {
 // MongoDB ATLAS
 // const uriDB = process.env.MONGO_URI; 
 
-mongoose
-  .connect(
-    // uriDB, 
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nmjiwwv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
-    {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() =>{
-    app.listen(5000);
-    console.log("LOG - Server running on port", 5000);
-  })
-  .catch(err => {
-    console.log(err);
-    console.log("LOG - Server failed to connect");
-  });
+// mongoose
+//   .connect(
+//     // uriDB, 
+//     // `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nmjiwwv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+//     // `mongodb+srv://eziokittu:southpoint19@cluster0.nmjiwwv.mongodb.net/rnpsoft-employee-portal?retryWrites=true&w=majority`,
+//     // 'mongodb+srv://user:user@cluster0.nmjiwwv.mongodb.net/rnpsoft-employee-portal?retryWrites=true&w=majority&appName=Cluster0',
+//     'mongodb+srv://user:user@cluster0.pmv3167.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0',
+//     // 'mongodb://127.0.0.1:27017/rnpsoft_employee_portal',
+//     {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   })
+//   .then(() =>{
+//     app.listen(5000);
+//     console.log("LOG - Server running on port", 5000);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//     console.log("LOG - Server failed to connect");
+//   });
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://user:user@cluster0.pmv3167.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
