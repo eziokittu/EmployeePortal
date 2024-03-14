18075c34a5e209d491d5cdab238bf10b3cc99b24
Bodhisatta Bhattacharjee <bodhisatta1999@gmail.com>
2024-03-15 02:45:20 +0530

    project controller needs to be worked on

--- server/models/project.js
+++ server/models/project.js
@@ -6,8 +6,9 @@ const projectSchema = new Schema({
   title: {type: String, required: true, default: "Project Title" },
   description: {type: String, required: true, default: "-" },
   review: {type: String, required: true, default: "-" },
-  srs: [{type: String, required: true, minLength: 4 }],
+  srs: {type: String, required: true, minLength: 4, default: process.env.DB_USER_DEFAULT_SRS },
   isCompleted: {type: Boolean, required: true, default: false},
+  domain: { type: mongoose.Types.ObjectId, required: true, ref: 'Domain', default: '-' },
   employees: [{ type: mongoose.Types.ObjectId, required: true, ref: 'User' }],
   date_start: {
     type: Date,
