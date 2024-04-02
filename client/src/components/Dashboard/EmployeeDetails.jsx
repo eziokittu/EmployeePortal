1efbfcba826a85b5e7518c3b7cbaf29a6c746652
Bodhisatta Bhattacharjee <bodhisatta1999@gmail.com>
2024-04-02 12:56:25 +0530

    fix1

--- client/src/components/Dashboard/EmployeeDetails.jsx
+++ client/src/components/Dashboard/EmployeeDetails.jsx
@@ -120,13 +120,16 @@ const EmployeeDetails = () => {
 						<span className="pl-8 my-4 text-lg font bold">RefID: {employeeDetails.ref}</span>
 
 						{/* Mobile No */}
-						<button href="#" className="flex justify-between items-center ml-8 mt-2 px-4 py-2 text-sm font-medium text-center text-white  bg-blue-700 rounded-lg hover:bg-blue-800 ">
+						<button href="#" className="flex justify-between items-center ml-8 mt-2 px-4 py-2 text-sm font-medium text-center text-white  bg-blue-700 rounded-lg cursor-default ">
 							<svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 32 32" id="Phone"><path d="M13.216 8.064c-.382-4.394-4.966-6.55-5.16-6.638a.974.974 0 0 0-.582-.078c-5.292.878-6.088 3.958-6.12 4.086a.99.99 0 0 0 .02.54c6.312 19.584 19.43 23.214 23.742 24.408.332.092.606.166.814.234a.99.99 0 0 0 .722-.042c.132-.06 3.248-1.528 4.01-6.316a.997.997 0 0 0-.096-.612c-.068-.132-1.698-3.234-6.218-4.33a.977.977 0 0 0-.884.21c-1.426 1.218-3.396 2.516-4.246 2.65-5.698-2.786-8.88-8.132-9-9.146-.07-.57 1.236-2.572 2.738-4.2a.998.998 0 0 0 .26-.766z" fill="#ffffff"></path></svg>
 							<span className='ms-3 text-md'>{employeeDetails.phone}</span>
+							{employeeDetails.isMobileOtpVerified && (
+								<span className='ms-3 text-md pl-4 text-green-500 font-bold text-lg'>Verified</span>
+							)}
 						</button>
 
 						{/* Email */}
-						<button href="#" className="flex justify-between items-center ml-8 mt-2 px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 ">
+						<button href="#" className="flex justify-between items-center ml-8 mt-2 px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg cursor-default ">
 							<svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 0 128 96" id="Email"><g data-name="Layer 2" fill="#ffffff" className="color000000 svgShape"><path d="M0 11.283V8a8 8 0 0 1 8-8h112a8 8 0 0 1 8 8v3.283l-64 40zm66.12 48.11a4.004 4.004 0 0 1-4.24 0L0 20.717V88a8 8 0 0 0 8 8h112a8 8 0 0 0 8-8V20.717z" fill="#ffffff" className="color000000 svgShape"></path></g></svg>
 							<span className='ms-3 text-md'> {employeeDetails.email}</span>
 						</button>
