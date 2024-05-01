9ddf838476c17781a9245b3d5072d58d8903463d
Bodhisatta Bhattacharjee <bodhisatta1999@gmail.com>
2024-05-01 12:59:24 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -73,10 +73,10 @@ app.use((error, req, res, next) => {
 
 const port = process.env.DB_PORT || 5000;
 // MongoDB ATLAS
-// const uriDB = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nmjiwwv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`; 
+const uriDB = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nmjiwwv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`; 
 
 // MongoDB Community Server
-const uriDB = 'mongodb://127.0.0.1:27017/rnpsoft_employee_portal'; 
+// const uriDB = 'mongodb://127.0.0.1:27017/rnpsoft_employee_portal'; 
 
 mongoose
   .connect(
